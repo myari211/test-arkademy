3ca0b142366f17b6d4aab4a6ebd8a9af1d1e790a
myari211 <apputra16@gmail.com>
2020-02-15 01:44:43 -0800

    add frontend

---
+++ soal-6/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import SearchBar from './comp/searchBar';
+import "./bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+function App() {
+  return (
+    <SearchBar />
+  );
+}
+
+export default App;
